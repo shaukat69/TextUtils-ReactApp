d7e3b5b0c21f14aabab1d840f7b999748e0c8688
Shaukat Ali <87563848+shauky6963@users.noreply.github.com>
2022-05-09 21:18:21 +0530

    First Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = (props) => {
+
+    return (
+            <footer className="d-flex flex-wrap justify-content-center py-3 bg-dark navbar-dark">
+                <div className="col-md-4 d-flex flex-column align-items-center">
+                    <span className="text-light">Copyright © 2022 TextUtils App</span>
+                    <a className='nav-link text-light' href="https://wa.me/+919923086963">Developed By Mr.Shaukat Ali.M.Hamdule</a>
+                </div>
+            </footer>
+    )
+}
+
+export default Footer
